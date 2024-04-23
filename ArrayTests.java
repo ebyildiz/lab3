73882f4e6f41e4f4e400c798db6e70ae5577b2bd
ebyildiz <eyildiz@ucsd.edu>
2024-04-23 15:28:09 -0700

    fixed errors

--- ArrayTests.java
+++ ArrayTests.java
@@ -1,5 +1,6 @@
 import static org.junit.Assert.*;
-import org.junit.*;
+
+import org.junit.Test;
 
 public class ArrayTests {
 	@Test 
@@ -14,5 +15,17 @@ public class ArrayTests {
   public void testReversed() {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+  }
+
+  @Test
+  public void testReversed1(){
+    int[] arr = {2,3,4,5};
+    assertArrayEquals(new int[]{5,4,3,2 }, ArrayExamples.reversed(arr));
+  }
+
+  @Test
+  public void testReverseInPlace2(){
+    int[] arr = {2,3,4,5};
+    assertArrayEquals(new int[]{5,4,3,2 }, ArrayExamples.reversed(arr));
   }
 }
